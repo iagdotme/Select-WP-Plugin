d95c56717674a3dd054f4ad59e802eacae530922
Ian Anderson Gray <ian@iag.me>
2013-10-22 17:46:39 +0100

    HTML5 caption shortcode
    Make captions HTML5 compliant and responsive

--- select-wp.php
+++ select-wp.php
@@ -309,7 +309,39 @@ $content = $featuredImage."<p>".get_the_excerpt()."</p>".'<p><a href="' . get_pe
 
 add_filter('the_excerpt_rss','feed_copyright_disclaimer'); 
 add_filter('the_content_feed','feed_copyright_disclaimer');
+// ------------------------------------------------------------------------------
 
+
+
+/**
+ * Filter to replace the [caption] shortcode text with HTML5 compliant code
+ *
+ * @return text HTML content describing embedded figure
+ **/
+function my_img_caption_shortcode_filter($val, $attr, $content = null)
+{
+    extract(shortcode_atts(array(
+        'id'    => '',
+        'align' => '',
+        'width' => '',
+        'caption' => ''
+    ), $attr));
+
+    if ( 1 > (int) $width || empty($caption) )
+        return $val;
+
+    $capid = '';
+    if ( $id ) {
+        $id = esc_attr($id);
+        $capid = 'id="figcaption_'. $id . '" ';
+        $id = 'id="' . $id . '" aria-labelledby="figcaption_' . $id . '" ';
+    }
+
+    return '<figure ' . $id . 'class="wp-caption ' . esc_attr($align) . '" >'
+    . do_shortcode( $content ) . '<figcaption ' . $capid 
+    . 'class="wp-caption-text">' . $caption . '</figcaption></figure>';
+}
+add_filter('img_caption_shortcode', 'my_img_caption_shortcode_filter',10,3);
 
 
 
